2a360692c906a1a9af18c2f8956ce45a3ee39ea4
Nika <nikaonime@gmail.com>
2023-06-27 19:42:09 +0300

    fetch, tmdb movie

--- src/components/Add.js
+++ src/components/Add.js
@@ -3,10 +3,21 @@ import React, { useState } from 'react'
 const Add = () => {
 
     const [query, setQuery] = useState('')
+    const [results, setResults] = useState([])
 
-    const onChange = (e) =>{
+    const onChange = (e) => {
         e.preventDefault()
         setQuery(e.target.value)
+
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=c59f2a1dc44f5f52bf7cb6a99c494f39&langauge=en-US&page=1&include_adult=false&query=${e.target.value}`)
+            .then((res) => res.json())
+            .then((data) => {
+                if (!data.errors) {
+                    setResults(data.results)
+                } else {
+                    setResults([])
+                }
+            })
     }
 
     return (
@@ -15,10 +26,20 @@ const Add = () => {
                 <div className='add-content'>
                     <div className='input-wrapper'>
                         <input
-                        value={query}
-                        onChange={onChange}
-                        type='text' placeholder='Search for a movie'/>
+                            value={query}
+                            onChange={onChange}
+                            type='text' placeholder='Search for a movie'
+                        />
                     </div>
+                    {results.length > 0 && (
+                        <ul className='results'>
+                            {results.map(movie=>(
+                                <li>
+                                    {movie.title}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
                 </div>
             </div>
         </div>
