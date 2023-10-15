f49cbcb01b55d4f3af1a61a1978010b68677620d
nika <nikaonime@gmail.com>
2023-10-15 09:31:56 +0300

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import {  Routes, Route } from "react-router-dom"
-import './lib/font-awesome/css/all.min.css'
+import { Routes, Route } from 'react-router-dom';
+import './lib/font-awesome/css/all.min.css';
 import Watched from './components/Watched';
 import Watchlist from './components/Watchlist';
 import Add from './components/Add';
@@ -8,16 +8,16 @@ import Header from './components/Header';
 import { GlobalProvider } from './context/GlobalState';
 
 function App() {
-  return (
-    <GlobalProvider>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Watchlist />} />
-        <Route path="/watched" element={<Watched />} />
-        <Route path="/add" element={<Add />} />
-      </Routes>
-    </GlobalProvider>
-  );
+    return (
+        <GlobalProvider>
+            <Header />
+            <Routes>
+                <Route path="/watchlist" element={<Watchlist />} />
+                <Route path="/watched" element={<Watched />} />
+                <Route path="/add" element={<Add />} />
+            </Routes>
+        </GlobalProvider>
+    );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
-            <div className='container'>
-                <div className='inner-content'>
-                    <div className='brand'>
-                        <Link to="/">Watchlist</Link>
+            <div className="container">
+                <div className="inner-content">
+                    <div className="brand">
+                        <Link to="/watchlist">Watchlist</Link>
                     </div>
-                    <ul className='nav-links'>
+                    <ul className="nav-links">
                         <li>
-                            <Link to="/">Watch list</Link>
+                            <Link to="/watchlist">Watch list</Link>
                         </li>
                         <li>
                             <Link to="/watched">Watched</Link>
                         </li>
                         <li>
-                            <Link to="/add" className='btn'>+ Add</Link>
+                            <Link to="/add" className="btn">
+                                + Add
+                            </Link>
                         </li>
                     </ul>
                 </div>
             </div>
         </header>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
